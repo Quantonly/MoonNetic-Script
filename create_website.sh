1c1d986143ee24fb9b8bed2b1e09584b99a0c3cf
Cody Volz <quantonly.dev@gmail.com>
2022-05-23 21:47:07 +0200

    Logs

--- create_website.sh
+++ create_website.sh
@@ -24,27 +24,27 @@ sudo ln -s /home/$2/$2 /var/www/$2
 #Virtual Host
 sudo touch /etc/apache2/sites-available/$2.moonnetic.com.conf
 sudo chmod 777 /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     ServerAdmin admin@$2.moonnetic.com" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     ServerName $2.moonnetic.com" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     ServerAlias www.$2.moonnetic.com" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     DocumentRoot /var/www/$2/public" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
-sudo echo "     CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-available/$2.moonnetic.com.com
-sudo echo "</VirtualHost>" >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '<VirtualHost *:80>' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     ServerAdmin admin@$2.moonnetic.com' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     ServerName $2.moonnetic.com' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     ServerAlias www.$2.moonnetic.com' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     DocumentRoot /var/www/$2/public' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     ErrorLog \${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
+sudo echo '     CustomLog \${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/$2.moonnetic.com.com
+sudo echo '</VirtualHost>' >> /etc/apache2/sites-available/$2.moonnetic.com.conf
 sudo a2ensite $2.moonnetic.com.conf
 sudo systemctl restart apache2
 
 #SFTP
 sudo chmod 777 /etc/ssh/sshd_config
-sudo echo "Match User $3" >> /etc/ssh/sshd_config
-sudo echo "     ForceCommand internal-sftp" >> /etc/ssh/sshd_config
-sudo echo "     PasswordAuthentication yes" >> /etc/ssh/sshd_config
-sudo echo "     ChrootDirectory /home/$2" >> /etc/ssh/sshd_config
-sudo echo "     PermitTunnel no" >> /etc/ssh/sshd_config
-sudo echo "     AllowAgentForwarding no" >> /etc/ssh/sshd_config
-sudo echo "     AllowTcpForwarding no" >> /etc/ssh/sshd_config
-sudo echo "     X11Forwarding no" >> /etc/ssh/sshd_config
+sudo echo 'Match User $3' >> /etc/ssh/sshd_config
+sudo echo '     ForceCommand internal-sftp' >> /etc/ssh/sshd_config
+sudo echo '     PasswordAuthentication yes' >> /etc/ssh/sshd_config
+sudo echo '     ChrootDirectory /home/$2' >> /etc/ssh/sshd_config
+sudo echo '     PermitTunnel no' >> /etc/ssh/sshd_config
+sudo echo '     AllowAgentForwarding no' >> /etc/ssh/sshd_config
+sudo echo '     AllowTcpForwarding no' >> /etc/ssh/sshd_config
+sudo echo '     X11Forwarding no' >> /etc/ssh/sshd_config
 sudo systemctl restart ssh
 
 #PhpMyAdmin
