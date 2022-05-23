860cf02dd078b12756a5b5aedb36412798338763
Cody Volz <quantonly.dev@gmail.com>
2022-05-23 22:41:28 +0200

    Set storage permissions script

---
+++ set_storage_permissions.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Start SSH connection
+sshpass -p "abc123!" ssh -o StrictHostKeyChecking=no admin05@$1 << EOF
+
+sudo -S chmod -R 777 /home/$2/$2/storage
+abc123!
+
+#End SSH connection
+EOF
+
